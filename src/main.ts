26c1438b98308f1b2c4b0d1dde67c081453e53c3
xchxn <seokregi@gmail.com>
2024-12-14 16:09:15 +0900

    post update

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3002);
+  await app.listen(3000);
 }
 bootstrap();
